195526335fd7dbf00f5de4dbf42c672ae19abf6f
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-07-19 22:26:28 +0200

    Added the weird method again

--- src/serialize.h
+++ src/serialize.h
@@ -820,6 +820,20 @@ public:
         else
             vch.insert(it, first, last);
     }
+#ifdef __linux__
+    void insert(iterator it, std::vector<char>::const_iterator first, std::vector<char>::const_iterator last)
+    {
+        assert(last - first >= 0);
+        if (it == vch.begin() + nReadPos && (unsigned int)(last - first) <= nReadPos)
+        {
+            // special case for inserting at the front when there's room
+            nReadPos -= (last - first);
+            memcpy(&vch[nReadPos], &first[0], last - first);
+        }
+        else
+            vch.insert(it, first, last);
+    }
+#endif
 
 #if !defined(_MSC_VER) || _MSC_VER >= 1300
     void insert(iterator it, const char* first, const char* last)

2ee74d43fb2f4bae1ae86cb93ac7cf71a119a3eb
Jan-Henrik <jh.bruhn@me.com>
2014-07-22 14:28:48 +0200

    Added cmdline device support to os x buildscript.

--- scripts/buildMac.sh
+++ scripts/buildMac.sh
@@ -2,8 +2,27 @@
 
 set -e
 
+rm -rf release/Mac/
+mkdir -p release/Mac
+
+echo "Let's start with the Commandline wallet device:"
+cd src/
+
+echo ""
+echo "Making the device on 4 cores..."
+make -f makefile.osx -j4 USE_UPNP=1
+
+echo ""
+echo "Releasing da shit."
+cp ./pedacoin ../release/Mac/
+
+cd ../
+echo "Done with the Commandline device."
+
+echo ""
+echo "Here comes the QT Wallet!"
 echo "Making Makefile..."
-qmake -config release
+qmake -config release "RELEASE=1 USE_UPNP=1"
 
 echo ""
 echo "Compiling on 4 cores..."
@@ -13,11 +32,9 @@ echo ""
 echo "Bundling..."
 macdeployqt PedaCoin.app -verbose=2
 
-rm -rf release/Mac/
 
 echo ""
 echo "Zipping..."
-mkdir -p release/Mac
 zip -r release/Mac/PedaCoin-Mac.zip PedaCoin.app
 
 mv PedaCoin.app release/Mac/
